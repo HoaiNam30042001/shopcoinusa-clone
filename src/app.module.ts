82786c9832ff2ef4ce8e561b06dc3b99acf3faba
Pham Thanh Phong <ptp150202@gmail.com>
2023-12-25 14:54:48 +0700

    GetAllUser, GetUserById

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/shopcoin'),
+    MongooseModule.forRoot(''), //Link localhost cua MongoDb
     UserModule
   ],
   controllers: [AppController],
