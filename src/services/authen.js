8149796e9ac1b5ed9549fcbdaa9388d5893d0acf
HOAI NAM <94825613+nhhoainam@users.noreply.github.com>
2024-01-04 17:24:17 +0700

    Delete src directory

--- src/services/authen.js
+++
@@ -1,8 +0,0 @@
-export const LoginServer = ({email,password,setIsProcess})=>{
-    try {
-        console.log("axios")
-        console.log("Email " +email+" Password "+ password)
-    } catch(err) {
-        setIsProcess(true)
-    }
-}
